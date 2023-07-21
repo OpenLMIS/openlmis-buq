b48528ab71bb7c55f3c925212f18186e07f3e5e8
Szymon Radziszewski <sradziszewski@soldevelo.com>
2023-07-21 09:39:49 +0200

    TZUP-457: Added Source Of Fund model

--- src/main/resources/db/migration/20170705125033654__initial_schema.sql
+++ src/main/resources/db/migration/20170705125033654__initial_schema.sql
@@ -6,3 +6,17 @@ CREATE TABLE widget (
     id uuid NOT NULL,
     name text NOT NULL
 );
+
+--
+-- Name: sources_of_funds; Type: TABLE; Schema: buq; Owner: postgres; Tablespace:
+--
+
+CREATE TABLE sources_of_funds (
+    id uuid NOT NULL,
+    name text NOT NULL,
+    description text NOT NULL
+);
+
+CREATE UNIQUE INDEX unq_source_of_fund_name
+ON sources_of_funds (LOWER(name))
+WHERE name IS NOT NULL;
