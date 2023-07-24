a96261f728a507cf6226292c0f9ecf238efef837
kchelstowski <kchelstowski@soldevelo.com>
2023-07-24 08:45:59 +0200

    TZUP-449 Add error handling for validation exception

--- src/main/java/org/openlmis/buq/errorhandling/GlobalErrorHandling.java
+++ src/main/java/org/openlmis/buq/errorhandling/GlobalErrorHandling.java
@@ -24,6 +24,9 @@ import org.openlmis.buq.i18n.MessageKeys;
 import org.openlmis.buq.util.Message;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -80,6 +83,24 @@ public class GlobalErrorHandling extends AbstractErrorHandling {
     }
 
     return getLocalizedMessage(new Message(ex.getMessage()));
+  }
+
+  /**
+   * Handles validation exceptions.
+   * @param ex the validation exception.
+   * @return the user-oriented error message.
+   */
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+          MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return ResponseEntity.badRequest().body(errors);
   }
 
 }
